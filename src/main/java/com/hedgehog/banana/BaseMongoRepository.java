c5481527ed812a4f364dd8fc2e8834226a183313
jonflacke <jonflacke@yahoo.com>
2022-03-16 11:11:52 -0400

    Modified the query system to work better with MongoDB

--- src/main/java/com/hedgehog/banana/BaseMongoRepository.java
+++ src/main/java/com/hedgehog/banana/BaseMongoRepository.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  * Created by Jon on 1/19/2019.
  */
 @NoRepositoryBean
-public interface BaseMongoRepository<T, ID extends Serializable> extends MongoRepository<T, ID>, BaseRepository<T, ID> {
+public interface BaseMongoRepository<T, ID extends Serializable> extends MongoRepository<T, ID> {
     default T findOne(ID id) {
         return (T) findById(id).orElse(null);
     }
